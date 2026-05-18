39c0ddff74b70cb6c6a02ba698e9b9aa11ff8781
Teromax34 <teromax34@gmail.com>
2026-05-18 18:36:37 -0300

    Update logicaAnalisis.py
    agregado clasificacion en dataset

--- logicaAnalisis.py
+++ logicaAnalisis.py
@@ -1,6 +1,16 @@
 A = [101, 102, 103, 104, 105, 106]
 B = [104, 105, 106, 107, 108]
 C = [102, 105, 109]
+criticos = []
+noCriticos = []
 for i in range(101,109):
-    print(i, "= ", ((i in A)or(i in B))and(i in C))
+    if ((i in A)or(i in B))and(i in C) == True:
+        criticos.append(i)
+    else:
+        noCriticos.append(i)
+
+print(criticos)
+print(noCriticos)
+
+
 
