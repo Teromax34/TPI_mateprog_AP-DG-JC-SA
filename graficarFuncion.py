6e8737eab56f4ba5293c0ed3c9998ecdade55227
Teromax34 <teromax34@gmail.com>
2026-05-19 14:10:23 -0300

    Update graficarFuncion.py
    agregado las otras funciones

--- graficarFuncion.py
+++ graficarFuncion.py
@@ -2,12 +2,18 @@ import matplotlib.pyplot as plt
 
 x = []
 a = []
+b = []
+c = []
 
 for i in range(0,50):
     x.append(i)
 
     a.append(40*i + 200)
+    b.append(70*i + 50)
+    c.append(-2*i**2 + 80*i + 100)
 
 plt.plot(x,a)
+plt.plot(x,b)
+plt.plot(x,c)
 
 plt.show()
