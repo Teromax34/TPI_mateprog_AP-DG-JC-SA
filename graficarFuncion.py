699053ad9fc0226ca442fc47a5c7c59e9b12d6e3
Teromax34 <teromax34@gmail.com>
2026-05-19 14:18:38 -0300

    Update graficarFuncion.py
    lo puse mas bonito

--- graficarFuncion.py
+++ graficarFuncion.py
@@ -12,8 +12,9 @@ for i in range(0,50):
     b.append(70*i + 50)
     c.append(-2*i**2 + 80*i + 100)
 
-plt.plot(x,a)
-plt.plot(x,b)
-plt.plot(x,c)
-
+plt.plot(x,a,label="40x+200")
+plt.plot(x,b,label="70x+50")
+plt.plot(x,c,label="-2x^2+80x+100")
+plt.grid(True)
+plt.legend()
 plt.show()
